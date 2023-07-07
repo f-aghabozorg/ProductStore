69cec1bfbc9081a023c19a3e82b96d51e3b1ecb9
fatemeaghabozorg <fatemeaghabozorg@yahoo.com>
2023-07-07 21:32:20 +0330

    chore(domain): Create entity class to domain layer
    - Add ER Diagram file to Diagram folder in Product_Store.Common
    - Add entity classes to Enties folder in Product_Store.Domain
    
    The added entities include:
    -[Manufacture]: this entity represents a registered user with capability of add or edit a product
    -[Product]: this entity represents a product which has a Manufacture
    
    These Entitieshave been carefully designed and implemented related to projects requirements and business logic.

---
+++ Product_Store.Domain/Entities/Products/Product.cs
@@ -0,0 +1,22 @@
+﻿using Product_Store.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Store.Domain.Entities.Products
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime ProduceDate { get; set; }
+        public string ManufactureEmail { get; set; }
+        public string ManufacturePhone { get; set; }
+
+        public virtual Manufacture Manufacture { get; set; }
+        public long? ManufactureId { get; set; }
+
+    }
+}
