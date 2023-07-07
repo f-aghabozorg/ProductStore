2d2851bbfdcd4f0cfe40143f7007c2d440dba55f
fatemeaghabozorg <fatemeaghabozorg@yahoo.com>
2023-07-07 22:05:36 +0330

    chore: delete unnecessary files
    Description:
    - Removed extra or redundant files from solution that no longer needed
    - add some changes to added files in Diagram folder in Common Layer

---
+++ Product_Store.Persistence/Context/DataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Store.Persistence.Context
+{
+    public class DataBaseContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
